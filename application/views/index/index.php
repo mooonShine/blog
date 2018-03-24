91fa1cd20f820025021f4ad955a7babeea68b1f6
夕夜 <969487286@qq.com>
2018-03-24 11:47:13 +0800

    首页修改

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -64,41 +64,41 @@
 						<div class="box-item">
 							<img src="/images/13.jpg" class="img-responsive"/>
 							<div class="content">
-								<h3>Pellentesque vehicula</h3>
-								<p>Nunc eu velit metus. Donec in massa libero. Donec bibendum orci a lorem scelerisque luctus. Aliquam et ante quis erat semper pretium. Pellentesque vehicula.</p>
+								<h3>swoole基础-TCP/IP和UDP</h3>
+								<p>平时我们打开一个浏览器，然后输入网址后回车，即展现了一个网页的内容。这是一个非常简单的操作。我们来简单的概括下背后的逻辑。浏览器通过TCP/IP协议建立到服务器的TCP连接客户端向...</p>
 								<a href="/index/single">Read More...</a>
 								<br><br>
-								<span>MAY 21, 2014 BY VAFPRESS</span><br>
+								<span>2 21, 2018 BY 夕夜</span><br>
 							</div>
 						</div>
 						<div class="box-item">
 							<img src="/images/1.jpg" class="img-responsive"/>
 							<div class="content">
-								<h3>Planning Our Honeymoon</h3>
-								<p>Suspendisse porttitor sapien ac lectus euismod imperdiet. Curabitur nec nibh at massa pellentesque accumsan eu id nibh. Donec accumsan ut mi et tincidunt. Aliquam eget metus nec leo tempor bibendum. Phasellus tincidunt lobortis metus. Duis euismod lorem turpis at diam.</p>
+								<h3>YII2 操作MongoDb/h3>
+								<p>1.增$data['id'] = 1;$data['title'] = 'YII2  操作MongoDb';$collec...</p>
 								<a href="/index/single">Read More...</a>
 								<br><br>
-								<span>MAY 21, 2014 BY VAFPRESS</span><br>
+								<span>2 10, 2018 BY 夕夜/span><br>
 							</div>
 						</div>
 						<div class="box-item">
 							<img src="/images/2.jpg" class="img-responsive"/>
 							<div class="content">
-								<h3>Brainstorming Wedding Ideas</h3>
-								<p>Suspendisse porttitor. Aliquam eget metus nec leo tempor bibendum. Phasellus tincidunt lobortis metus. Duis euismod lorem turpis, viverra feugiat ipsum volutpat vitae. Fusce justo turpis, sodales sed placerat non, suscipit at diam.</p>
+								<h3>用户管理命令详解</h3>
+								<p>useradd -u 1000 user1userdd -g hellosa user2-u UID-g GID 基本组-G GID 附加组-c 住宿-d /path/to/dir...</p>
 								<a href="/index/single">Read More...</a>
 								<br><br>
-								<span>MAY 21, 2014 BY VAFPRESS</span><br>
+								<span>12 7, 2017 BY 夕夜/span><br>
 							</div>
 						</div>
 						<div class="box-item">
 							<img src="/images/10.jpg" class="img-responsive"/>
 							<div class="content">
-								<h3>Choosing The Bridesmaids and Groomsmen</h3>
-								<p>Suspendisse porttitor sapien ac lectus euismod imperdiet. Curabitur nec nibh at massa pellentesque accumsan eu id nibh. Donec accumsan ut mi.</p>
+								<h3>Python 环境搭建</h3>
+								<p>1.Python 可以跨平台适合的系统有 LInux Windows Mac2.7版 -----------语法不兼容-------------------3.3版2.7版本库比较...</p>
 								<a href="/index/single">Read More...</a>
 								<br><br>
-								<span>MAY 21, 2014 BY VAFPRESS</span><br>
+								<span>11 25, 2017 BY 夕夜</span><br>
 							</div>
 						</div>
 					</div>
@@ -106,41 +106,41 @@
 						<div class="box-item">
 							<img src="/images/3.jpg" class="img-responsive"/>
 							<div class="content">
-								<h3>Donec bibendum orci</h3>
-								<p>Nunc eu velit metus. Donec in massa libero. Donec bibendum orci a lorem scelerisque luctus. Aliquam et ante quis erat semper pretium. Pellentesque vehicula.</p>
+								<h3>python 简介</h3>
+								<p>1.python 适合的领域web 网站和各种网络服务系统工具和脚本作为胶水语言和其他语言开发的模块包装起来方便使用。2.python不适合的领域贴近硬件的代码（首选C）移动开发：...</p>
 								<a href="/index/single">Read More...</a>
 								<br><br>
-								<span>MAY 21, 2014 BY VAFPRESS</span><br>
+								<span>11 20, 2017 BY 夕夜</span><br>
 							</div>
 						</div>
 						<div class="box-item">
 							<img src="/images/4.jpg" class="img-responsive"/>
 							<div class="content">
-								<h3>Met Dovey’s Parents in Chicago</h3>
-								<p>Class aptent taciti urna bibendum id. Duis a commodo lectus. Morbi id purus nec purus feugiat vestibulum. Suspendisse sapien ante, bibendum ac quam quis, imperdiet bibendum sem. Curabitur nibh magna, tristique et convallis…</p>
+								<h3>swoole基础-与传统MVC框架的整合/h3>
+								<p>最近很多人私下留言给我，问swoole与yii2怎么整合？由于yii2本身就是传统MVC框架的典型代表之一，所以这个问题又可以等同于swoole如何与传统的MVC框架整合，比如la...</p>
 								<a href="/index/single">Read More...</a>
 								<br><br>
-								<span>MAY 21, 2014 BY VAFPRESS</span><br>
+								<span>11 4, 2017 BY 夕夜<</span><br>
 							</div>
 						</div>
 						<div class="box-item">
 							<img src="/images/8.jpg" class="img-responsive"/>
 							<div class="content">
-								<h3>Choosing The Bridesmaids and Groomsmen</h3>
-								<p>Suspendisse porttitor sapien ac lectus euismod imperdiet. Curabitur nec nibh at massa pellentesque accumsan eu id nibh. Donec accumsan ut mi.</p>
+								<h3> 支付宝网页授权获取用户信息/h3>
+								<p>1. 支付宝生活号申请，申请完成后上传 RSA2(SHA256)密钥2.  URL拼接与scope详解  获取auth_code  （支付宝...</p>
 								<a href="/index/single">Read More...</a>
 								<br><br>
-								<span>MAY 21, 2014 BY VAFPRESS</span><br>
+								<span>10 20, 2017 BY 夕夜</span><br>
 							</div>
 						</div>
 						<div class="box-item">
 							<img src="/images/11.jpg" class="img-responsive"/>
 							<div class="content">
-								<h3>She Said Yes!</h3>
-								<p>Nunc eu velit metus. Donec in massa libero. Donec bibendum orci a lorem scelerisque luctus. Aliquam et ante quis erat semper pretium. Pellentesque vehicula.</p>
+								<h3> 4 swoole基础-IO模型/h3>
+								<p>IO即Input/Output,输入和输出的意思。在计算机的世界里，涉及到数据交换的地方，比如磁盘、网络等，就需要IO接口。通常，IO是相对的。比如说你打开浏览器，通过网络IO获取...IO即Input/Output,输入和输出的意思。在计算机的世界里，涉及到数据交换的地方，比如磁盘、网络等，就需要IO接口。通常，IO是相对的。比如说你打开浏览器，通过网络IO获取...</p>
 								<a href="/index/single">Read More...</a>
 								<br><br>
-								<span>MAY 21, 2014 BY VAFPRESS</span><br>
+								<span>10 28, 2017 BY 夕夜</span><br>
 							</div>
 						</div>
 					</div>
@@ -148,41 +148,41 @@
 						<div class="box-item">
 							<img src="/images/5.jpg" class="img-responsive"/>
 							<div class="content">
-								<h3>Choosing The Bridesmaids and Groomsmen</h3>
-								<p>Suspendisse porttitor sapien ac lectus euismod imperdiet. Curabitur nec nibh at massa pellentesque accumsan eu id nibh. Donec accumsan ut mi.</p>
+								<h3>swoole基础-进程和线程</h3>
+								<p>进程进程和线程，涉及到操作系统的基本知识，但是很多phper不懂，这是实话，因为php简单易学，大部分人在零基础学php编程的时候都没有去了解过这个东西。既然关系到操作系统，那跟我...</p>
 								<a href="/index/single">Read More...</a>
 								<br><br>
-								<span>MAY 21, 2014 BY VAFPRESS</span><br>
+								<span>10 13, 2017 BY 夕夜</span><br>
 							</div>
 						</div>
 						<div class="box-item">
 							<img src="/images/12.jpg" class="img-responsive"/>
 							<div class="content">
-								<h3>She Said Yes!</h3>
-								<p>Nunc eu velit metus. Donec in massa libero. Donec bibendum orci a lorem scelerisque luctus. Aliquam et ante quis erat semper pretium. Pellentesque vehicula.</p>
+								<h3>Mysql 主从复制的原理/h3>
+								<p>一、什么是主从复制至少需要2台mysql服务器，一台配置为主服务器，一台配置为从服务器，主服务器的数据要同步到从服务器。二、主从复制的原理mysql中有一种日志，叫做bin日志（二...</p>
 								<a href="/index/single">Read More...</a>
 								<br><br>
-								<span>MAY 21, 2014 BY VAFPRESS</span><br>
+								<span>9 9, 2017 BY 夕夜</span><br>
 							</div>
 						</div>
 						<div class="box-item">
 							<img src="/images/6.jpg" class="img-responsive"/>
 							<div class="content">
-								<h3>A Sweet Escape to Paris</h3>
-								<p>Proin lobortis mattis odio non ornare. Sed tempor nisi eu hendrerit luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostravelit id sem tincidunt convallis ac eget quam. Vestibulum posuere porttitor sapien .</p>
+								<h3>PHP 常用函数集锦PHP 常用函数集锦</h3>
+								<p>/** * 方法描述：字符截取 支持UTF8/GBK * @param $string * @param int $length * @param string $dot * @p...</p>
 								<a href="/index/single">Read More...</a>
 								<br><br>
-								<span>MAY 21, 2014 BY VAFPRESS</span><br>
+								<span>8 22, 2017 BY 夕夜</span><br>
 							</div>
 						</div>
 						<div class="box-item">
 							<img src="/images/7.jpg" class="img-responsive"/>
 							<div class="content">
-								<h3>Met Dovey’s Parents in Chicago</h3>
-								<p>Class aptent taciti urna bibendum id. Duis a commodo lectus. Morbi id purus nec purus feugiat vestibulum. Suspendisse sapien ante, bibendum ac quam quis, imperdiet bibendum sem. Curabitur nibh magna, tristique et convallis…</p>
+                            <h3>如何判断扫码的是微信还是支付宝？</h3>
+								<p>主要通过 HTTP_USER_AGENT 来判断以PHP代码举例public function actionTest(){    $user_agent = $...</p>
 								<a href="/index/single">Read More...</a>
 								<br><br>
-								<span>MAY 21, 2014 BY VAFPRESS</span><br>
+								<span>7 25, 2017 BY 夕夜</span><br>
 							</div>
 						</div>
 					</div>
