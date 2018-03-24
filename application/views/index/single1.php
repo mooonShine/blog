a663e1926c60e91b9c498d500c5e39c0fa7c4879
夕夜 <969487286@qq.com>
2018-03-24 11:59:03 +0800

    调整

---
+++ application/views/index/single1.php
@@ -0,0 +1,97 @@
+<!-- Navigation -->
+	
+	<!-- Background Gradients-->
+	<div  class="site-gradients">
+		<div class="site-gradients-media">
+			<figure>
+				<img src="Another%20Yosemite%20short%20movie%20project%20%E2%80%93%20Modern_files/PcLGXNjMTdiFVKTrElCl__DSC2245.jpg" alt="PcLGXNjMTdiFVKTrElCl__DSC2245" srcset="https://themedemos.webmandesign.eu/modern/wp-content/uploads/sites/8/2014/11/PcLGXNjMTdiFVKTrElCl__DSC2245.jpg 1920w, https://themedemos.webmandesign.eu/modern/wp-content/uploads/sites/8/2014/11/PcLGXNjMTdiFVKTrElCl__DSC2245-420x280.jpg 420w, https://themedemos.webmandesign.eu/modern/wp-content/uploads/sites/8/2014/11/PcLGXNjMTdiFVKTrElCl__DSC2245-744x497.jpg 744w, https://themedemos.webmandesign.eu/modern/wp-content/uploads/sites/8/2014/11/PcLGXNjMTdiFVKTrElCl__DSC2245-1200x801.jpg 1200w" sizes="(max-width: 1617px) 100vw, 1617px" height="1080" width="1617">
+			</figure>
+		</div>
+	</div>
+
+	<header class="container">
+		<div class="site-branding">
+			<h1 class="site-title">
+				<a href="/index/index">
+					<span>Justice</span>
+				</a>
+			</h1>
+			<h2 class="site-description">Welcome to Us !</h2>
+		</div>
+	</header>
+	
+	<!-- /////////////////////////////////////////Content -->
+	<div id="page-content" class="single-page">
+		<div class="container">
+			<div class="row">
+				<div id="main-content">
+					<article>
+						<img src="/images/banner1.jpg" />
+						<div class="art-content">
+							<h1>Sharing Your Explorer’s Story: Man and Mother Nature</h1>
+							<div class="info">By <a href="#">Danny</a> on April 14, 2015</div>
+							<div class="excerpt"><p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum exercitation ullamco laboris nisi ut aliquip.</p></div>
+							<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+							 sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+							 invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+							<blockquote><p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet vultatup duista.</p></blockquote>
+							<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis
+							at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril.</p>
+							<h2>Heading 1</h2>
+							<p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
+							 Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse lorem ipsum dolor sit amet.</p>
+							<h2>Heading 2</h2>
+							<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis. 
+							 At vero eos et accusam et justo.</p>
+							<h2>Heading 3</h2>
+							<p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+							 At vero eos et accusam et justo duo dolores et ea rebum hendrerit in vulputate velit esse molestie.</p>
+							<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+							 sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+						
+							<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+							sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+							<div class="note">
+							  <ol>
+								<li>Lorem ipsum</li>
+								<li>Sit amet vultatup nonumy</li>
+								<li>Duista sed diam</li>
+							  </ol>
+							  <div class="clear"></div>
+							</div>
+							<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at 
+							 vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. 
+							 Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+							<p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores.</p>
+						</div>
+					</article>
+					<div class="widget wid-related">
+						<div class="heading"><h4>Related Post</h4></div>
+						<div class="content">
+							<div class="row">
+								<div class="col-md-4">
+									<div class="wrap-col">
+										<a href="#"><img src="/images/7.jpg" /></a>
+										<h4><a href="#">Vero eros et accumsan et iusto odio </a></h4>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="wrap-col">
+										<a href="#"><img src="/images/8.jpg" /></a>
+										<h4><a href="#">Vero eros et accumsan et iusto odio </a></h4>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="wrap-col">
+										<a href="#"><img src="/images/6.jpg" /></a>
+										<h4><a href="#">Vero eros et accumsan et iusto odio </a></h4>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
